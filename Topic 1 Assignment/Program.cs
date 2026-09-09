9a69a84f02a2a350fd88f4ebca25728af0ea007c
Said Said <363237421@GoTVDSB.ca>
2026-09-09 13:20:30 -0400

    Demo finished

--- Topic 1 Assignment/Program.cs
+++ Topic 1 Assignment/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Topic_1_Assignment
+﻿namespace Topic_1_Assignment //Said
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("My name is Said and this is my first C# assignment!");
+            Console.WriteLine("");
+            Console.WriteLine("Two reasons why comments are used are because they help explain the code and they can be used to temporarily disable code.");
+            Console.WriteLine("");
+            Console.WriteLine(" SSS    SSS");
+            Console.WriteLine("S   S  S   S");
+            Console.WriteLine("S      S    ");
+            Console.WriteLine(" SSS    SSS");
+            Console.WriteLine("    S      S");
+            Console.WriteLine("S   S  S   S");
+            Console.WriteLine(" SSS    SSS");
         }
     }
 }
